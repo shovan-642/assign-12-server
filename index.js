fc86fc5e623d043236dae7b9d1b28ae9706cfc33
shovan-642 <shovan642@gmail.com>
2025-04-30 20:29:15 +0600

    express, cors, jwt, middleware added

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 const express = require('express');
+const app = express();
+const cors = require('cors')
+const jwt = require('jsonwebtoken')
+require('dotenv').config();
+
+
+const port = process.env.PORT || 5000;
+
+
+app.use(cors())
+app.use(express.json())
+
+
+
+app.get('/', (req, res)=>{
+    res.send('my express server is running')
+})
+
+app.listen(port, ()=>{
+    console.log(`my port is ${port}` )
+})
