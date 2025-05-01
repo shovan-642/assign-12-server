17649786fd71cc153eb05a80b29c7a657730c988
shovan-642 <shovan642@gmail.com>
2025-05-01 12:15:25 +0600

    Stop tracking .env file

--- index.js
+++ index.js
@@ -41,10 +41,6 @@ async function run() {
         res.send(result)
     })
 
-    // Connect the client to the server	(optional starting in v4.7)
-    // await client.connect();
-    // Send a ping to confirm a successful connection
-    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
