738d8d8e2e330d1f23a94a2a3af45c7999dc5f08
shovan-642 <shovan642@gmail.com>
2025-05-01 12:04:03 +0600

    get camp data done

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const cors = require('cors')
 const jwt = require('jsonwebtoken')
 require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 
 const port = process.env.PORT || 5000;
@@ -10,6 +11,49 @@ const port = process.env.PORT || 5000;
 
 app.use(cors())
 app.use(express.json())
+
+// assignment-12
+// YQt$e!A~63x2QNv
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@assignment-10.9gvo9.mongodb.net/?retryWrites=true&w=majority&appName=assignment-10`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+
+    const campCollection = client.db("MCMS").collection("camps")
+
+
+
+    app.get("/camp", async(req, res)=>{
+        const result = await campCollection.find().toArray()
+        res.send(result)
+    })
+
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+    // Send a ping to confirm a successful connection
+    // await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+  }
+}
+run().catch(console.dir);
+
+
+
 
 
 
